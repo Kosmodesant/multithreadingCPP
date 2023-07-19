97f2b38c36ffc5ef97c7d62ed169e440594958ac
Kosmodesant <smirnov.nal@outlook.com>
2023-07-19 23:04:03 +0300

    release 1.0

---
+++ 02/01/task1/task1/task1.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <thread>
+#include <atomic>
+
+std::atomic<int> clientCounter = 0; // Atomic client counter
+const int MAX_CLIENTS = 10;         // максимальное число клиентов
+
+void clientThread()
+{
+	while (true)
+	{
+		if (clientCounter >= MAX_CLIENTS)
+		{
+			break;
+		}
+		++clientCounter;
+		std::cout << "Новый клиент! Всего клиентов: " << clientCounter << std::endl;
+		std::this_thread::sleep_for(std::chrono::seconds(1));
+	}
+}
+
+void operatorThread()
+{
+	while (true)
+	{
+		if (clientCounter <= 0)
+		{
+			break;
+		}
+		--clientCounter;
+		std::cout << "Обслужен клиент! Осталось клиентов: " << clientCounter << std::endl;
+		std::this_thread::sleep_for(std::chrono::seconds(2));
+	}
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	std::thread clientThreadObj(clientThread);
+	std::thread operatorThreadObj(operatorThread);
+
+	clientThreadObj.join();
+	operatorThreadObj.join();
+
+	return 0;
+}
